6a51b75cfc49571fa25244bc69a6b13759905517
TJeOrrMy <53700491+TJeOrrMy@users.noreply.github.com>
2019-08-04 21:42:09 +0800

    Add files via upload

---
+++ C Number/C Number/C Number.h
@@ -0,0 +1,32 @@
+﻿
+// C Number.h: PROJECT_NAME 应用程序的主头文件
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "在包含此文件之前包含 'pch.h' 以生成 PCH"
+#endif
+
+#include "resource.h"		// 主符号
+
+
+// CCNumberApp:
+// 有关此类的实现，请参阅 C Number.cpp
+//
+
+class CCNumberApp : public CWinApp
+{
+public:
+	CCNumberApp();
+
+// 重写
+public:
+	virtual BOOL InitInstance();
+
+// 实现
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CCNumberApp theApp;
---
+++ C Number/C Number/C NumberDlg.h
@@ -0,0 +1,71 @@
+﻿
+// C NumberDlg.h: 头文件
+//
+
+#pragma once
+
+
+// CCNumberDlg 对话框
+class CCNumberDlg : public CDialogEx
+{
+// 构造
+public:
+	CCNumberDlg(CWnd* pParent = nullptr);	// 标准构造函数
+
+// 对话框数据
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_CNUMBER_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 支持
+
+
+// 实现
+protected:
+	HICON m_hIcon;
+
+	// 生成的消息映射函数
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+public:
+//	CEdit m_P1;
+//	CEdit m_P2;
+//	int m_P3;
+//	CString m_P2;
+//	CString m_P3;
+//	CString m_P1;
+//	CString m_T1;
+//	CString m_T2;
+//	CEdit m_T3;
+//	CString m_M3;
+//	CString m_M1;
+//	CEdit m_M2;
+//	CString m_E1;
+//	CString m_M2;
+//	CString m_E2;
+//	CString m_E3;
+//	CString m_T3;
+	afx_msg void OnBnClickedPe();
+	afx_msg void OnBnClickedMe();
+	afx_msg void OnBnClickedTe();
+	afx_msg void OnBnClickedEe();
+	int m_E1;
+	int m_E2;
+	int m_E3;
+//	int m_M1;
+	int m_M1;
+	int m_M2;
+	int m_M3;
+	int m_P1;
+//	int m_P2;
+	int m_P2;
+	int m_P3;
+	int m_T1;
+	int m_T2;
+	int m_T3;
+//	CProgressCtrl m_Progress;
+};
